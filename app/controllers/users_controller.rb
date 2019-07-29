8a8f099be75a6735d2b6cc518bb93d3fc68dc249
claudiasolbes <cssolbes@hotmail.com>
2019-07-29 17:52:23 -0400

    deleted flights index, added flight show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,49 +1,50 @@
 class UsersController < ApplicationController
-    before_action :find_user, only: [:show, :edit, :update]
+  before_action :find_user, only: [:show, :edit, :update]
 
-    def index
-        @users = User.all
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user
+      @user.save
+      redirect_to users_path(@user)
+    else
+      render :new
     end
+  end
 
-    def show
+  def edit
+  end
+
+  def update
+    @user.update(user_params)
+    if @user
+      @user.save
+      redirect_to users_path(@user)
     end
+  end
 
-    def new
-        @user = User.new
-    end
-
-    def create 
-        @user = User.new(user_params)
-        if @user
-            @user.save
-            redirect_to users_path(@user)
-        else
-            render :new
-        end
-    end 
-    def edit
-    
-    end 
-
-    def update
-        @user.update(user_params)
-        if @user
-            @user.save
-            redirect_to users_path(@user)
-        end
-    end 
-
-    def destroy
-        @user = User.destroy(params[:id])
-        redirect_to users_path(@path)
-    end 
+  def destroy
+    @user = User.destroy(params[:id])
+    redirect_to users_path(@path)
+  end
 
 
-    private
-    def user_params
-        params.require(:user).permit(:name, :passport_number, :username, :password)
-    end
-    def find_user
-        @user = User.find_by(params[:id])
-    end
+  private
+  def user_params
+    params.require(:user).permit(:name, :passport_number, :username, :password)
+  end
+
+  def find_user
+    @user = User.find_by(params[:id])
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 # User.destroy_all
 # Trip.destroy_all
-# Country.destroy_all 
+# Country.destroy_all
 # Flight.destroy_all
 
 require 'faker'
@@ -8,8 +8,8 @@ require 'faker'
 
 
 user_counter = 0
-unless user_counter > 150 
-    20.times do 
+unless user_counter > 150
+    20.times do
         User.create(name:"#{Faker::Name.name}", passport_number: Faker::Number.number(9), username: "#{Faker::Internet.username}",password: "#{Faker::Internet.password}")
 
         User.create(name:"#{Faker::Name.name}", passport_number: Faker::Number.number(9), username: "#{Faker::Internet.username}",password: "#{Faker::Internet.password}")
@@ -26,8 +26,8 @@ end
 
 
 trip_counter = 0
-unless trip_counter > 150 
-    10.times do 
+unless trip_counter > 150
+    10.times do
         Trip.create(start_date: Faker::Date.between(100.days.ago, Date.today), end_date: Faker::Date.between(Date.today, 1.year.from_now), user_id: User.all.sample.id)
 
         Trip.create(start_date: Faker::Date.between(1.year.ago, 8.months.ago), end_date: Faker::Date.between( 8.months.ago, 9.months.ago), user_id: User.all.sample.id)
@@ -35,24 +35,24 @@ unless trip_counter > 150
 
         Trip.create(start_date: Faker::Date.between(1.year.ago, 6.months.ago), end_date: Faker::Date.between( 6.months.ago, 4.months.ago), user_id: User.all.sample.id)
 
-        trip_counter += 1 
+        trip_counter += 1
     end
 end
 
 
 
 country_counter = 0
-unless country_counter > 150 
-    25.times do 
-        Country.create(name: "#{Faker::Address.country}", currency: "#{Faker::Currency.code}")
-        
-        Country.create(name: "#{Faker::Address.country}", currency: "#{Faker::Currency.code}")
-        
+unless country_counter > 150
+    25.times do
         Country.create(name: "#{Faker::Address.country}", currency: "#{Faker::Currency.code}")
 
         Country.create(name: "#{Faker::Address.country}", currency: "#{Faker::Currency.code}")
 
-        country_counter += 1 
+        Country.create(name: "#{Faker::Address.country}", currency: "#{Faker::Currency.code}")
+
+        Country.create(name: "#{Faker::Address.country}", currency: "#{Faker::Currency.code}")
+
+        country_counter += 1
     end
 end
 
@@ -63,6 +63,6 @@ unless flight_counter > 100
         Faker::Name.initials}", destination_airport: "#{Faker::Name.initials}")
 
         Flight.create(flight_number: Faker::Number.number(4), country_id: Country.all.sample.id, trip_id: Trip.all.sample.id, origin_airport: "#{Faker::Name.initials}", destination_airport: "#{Faker::Name.initials}")
-        flight_counter += 1 
+        flight_counter += 1
     end
 end
