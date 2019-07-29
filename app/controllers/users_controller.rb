b31d2d4fccf0b46f0bff277db1999890f87bc3d4
claudiasolbes <cssolbes@hotmail.com>
2019-07-29 18:18:22 -0400

    activities migration, created modelm views and controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-<<<<<<< HEAD
+
   before_action :find_user, only: [:show, :edit, :update]
 
   def index
@@ -17,39 +17,26 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user
       @user.save
-      redirect_to users_path(@user)
+      redirect_to @user
     else
       render :new
-=======
-    # before_action :find_user, only: [:show, :edit, :update]
-
-    def index
-        @users = User.all
->>>>>>> dc9c39f7484672f7f26d44074c9cc5a0d553763a
     end
   end
 
-<<<<<<< HEAD
   def edit
   end
-=======
-    def show
-        @user = User.find(params[:id])
-    end
->>>>>>> dc9c39f7484672f7f26d44074c9cc5a0d553763a
 
   def update
     @user.update(user_params)
     if @user
       @user.save
-      redirect_to users_path(@user)
+      redirect_to @user
     end
   end
 
-<<<<<<< HEAD
   def destroy
     @user = User.destroy(params[:id])
-    redirect_to users_path(@path)
+    redirect_to users_path
   end
 
 
@@ -59,43 +46,6 @@ class UsersController < ApplicationController
   end
 
   def find_user
-    @user = User.find_by(params[:id])
+    @user = User.find(params[:id])
   end
-=======
-    def create 
-        @user = User.new(user_params)
-        if @user
-            @user.save
-            redirect_to @user
-        else
-            render :new
-        end
-    end 
-    def edit
-        @user = User.find(params[:id])
-    end 
-
-    def update
-        @user = User.find(params[:id])
-        @user.update(user_params)
-        if @user
-            @user.save
-            redirect_to @user
-        end
-    end 
-
-    def destroy
-        @user = User.destroy(params[:id])
-        redirect_to users_path
-    end 
-
-
-    private
-    def user_params
-        params.require(:user).permit(:name, :passport_number, :username, :password)
-    end
-    def find_user
-        @user = User.find_by(params[:id])
-    end
->>>>>>> dc9c39f7484672f7f26d44074c9cc5a0d553763a
 end
