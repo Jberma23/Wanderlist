e6615189c2d3bda76ceb74ac1316a15c95188430
claudiasolbes <cssolbes@hotmail.com>
2019-07-29 16:40:55 -0400

    country index links to show page

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,17 +1,20 @@
 class CountriesController < ApplicationController\
     before_action: :find_country, only: [:show]
-    
-    def index 
-    end 
+
+    def index
+      @countries = Country.all
+    end
+
     def show
-    end 
-    
+    end
+
     private
 
     def country_params
         params.require(:country).permit(:name, :currency)
-    end 
-    def find_country 
+    end
+
+    def find_country
         @country = Country.find_by(params[:id])
     end
 end
