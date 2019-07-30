96cda8550c7804eca05f73391b80ed8dcf79c55e
github username <github email address>
2019-07-30 16:30:01 -0400

    fixed?

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,10 @@
 class Flight < ApplicationRecord
     belongs_to :trip 
     belongs_to :country
+
+
+
+    def flight_info
+        "num: #{self.flight_number} origin: #{self.origin_airport} destination: #{self.destination_airport}"
+    end
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,5 +2,4 @@ class Trip < ApplicationRecord
     belongs_to :user
     has_many :flights
     has_many :countries, through: :flights
-    accepts_nested_attributes_for :flights
 end
