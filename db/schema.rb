29bf50ddf2cfc47f5394c5f82e397dfae5d7f432
claudiasolbes <cssolbes@hotmail.com>
2019-07-30 11:37:52 -0400

    added links to trips show page and changed format of user show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_30_132823) do
+ActiveRecord::Schema.define(version: 2019_07_30_151312) do
 
   create_table "activities", force: :cascade do |t|
     t.integer "user_id"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2019_07_30_132823) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   create_table "users", force: :cascade do |t|
